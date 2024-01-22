ad706436d19c04a448796b9678329331e9b1830e
atloyd <aloyd@AustinsPC>
2024-01-22 08:03:57 -0600

    completed workshop

--- index.js
+++ index.js
@@ -1,1 +1,79 @@
 // TODO: this file! :)
+
+let numArr = [];
+const odds = [];
+const evens = [];
+
+// Submit Form
+
+const form = document.querySelector("form");
+const bankOutput = document.querySelector("#bankOutput");
+
+form.addEventListener("submit", addNum);
+
+// Render
+
+function render() {
+  numArr.forEach((num) => {
+    const span = document.createElement("span");
+    span.textContent = num;
+  });
+
+  bankOutput.replaceChildren(...(numArr + ""));
+  evenOutput.replaceChildren(...(evens + ""));
+  oddsOutput.replaceChildren(...(odds + ""));
+}
+
+// Add a number
+
+function addNum(event) {
+  event.preventDefault();
+  const newNum = event.target.number.value;
+  numArr.push(newNum);
+  render();
+}
+
+// Sort One Number
+
+const sortOneNum = document.querySelector("#sortOne");
+sortOneNum.addEventListener("click", sortOne);
+const evenOutput = document.querySelector("#evenOutput");
+const oddsOutput = document.querySelector("#oddsOutput");
+
+function sortOne(event) {
+  event.preventDefault();
+  const shiftNum = numArr.shift();
+  if (shiftNum === undefined) {
+    return;
+  } else if (shiftNum % 2 === 0) {
+    evens.push(shiftNum);
+    console.log(evens);
+  } else {
+    odds.push(shiftNum);
+    console.log(odds);
+  }
+  render();
+}
+
+// Sort All Numbers
+
+const numberBank = document.querySelector("#sortAll");
+numberBank.addEventListener("click", sortAll);
+
+function sortAll(event) {
+  event.preventDefault();
+  numArr.forEach((num) => {
+    if (num === undefined) {
+      return;
+    } else if (num % 2 === 0) {
+      evens.push(num);
+    } else {
+      odds.push(num);
+    }
+  });
+  numArr = [];
+  //delete log lines
+  console.log(evens);
+  console.log(odds);
+  render();
+}
